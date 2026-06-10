32f50b962a2e6ba384c85b12a4756a98f25dd562
adilmannan <codesbyadil@gmail.com>
2026-06-10 23:50:40 +0530

    feat: implement user login functionality and validation

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser())
 
 // cors config
 app.use(
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { login, registerUser } from "../controllers/auth.controllers.js";
+import { validate } from "../middlewares/validator.middlewares.js";
+import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
 
 const router = Router();
 
 // router.get('/', healthCheck)
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+
+router.route("/login").post(userLoginValidator,validate,login);
 
 export default router;
